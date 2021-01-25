f6f2dcb37f1475838fe7c64440b233289d07494c
Gluhenko Daniil <gluhenko-dv@yandex.ru>
2021-01-25 03:36:52 +0300

    add banner

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,11 @@
 import React from "react";
 import dbMenu from "./DBMenu";
 import { ListItem } from "./ListItem";
+import {Banner} from './Banner';
 
 export const Menu = () => (
   <div className="menu">
+  <Banner/>
     <section className="menu-list">
       <h2>Бургеры</h2>
       <ListItem itemList={dbMenu.burger} />
